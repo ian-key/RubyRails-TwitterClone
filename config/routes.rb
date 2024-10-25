78b1860cc33c1168dc3044c140f1dbc81cb7dead
Ian <ian@iankey.co.uk>
2024-10-25 12:56:51 +0100

    App completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@ Rails.application.routes.draw do
   get '/feeds' => 'feeds#index'
 
   # USERS
-
+  post '/users' => 'users#create'
   # SESSIONS
-
+  post '/sessions' => 'sessions#create'
+  get '/authenticated' => 'sessions#authenticated'
+  delete '/sessions' => 'sessions#destroy'
   # TWEETS
-
+  get '/tweets' => 'tweets#index'
+  post '/tweets' => 'tweets#create'
+  delete 'tweets/:id' => 'tweets#destroy'
+  get '/users/:username/tweets' => 'tweets#index_by_user'
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
 end
---
+++ db/migrate/20241025101349_add_session_foreign_key.rb
@@ -0,0 +1,5 @@
+class AddSessionForeignKey < ActiveRecord::Migration[6.1]
+  def change
+    add_belongs_to :sessions, :user
+  end
+end
